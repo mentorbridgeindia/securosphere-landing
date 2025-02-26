6f4cd8fd46453f8674b6a065d7016ed697880b7d
Jagan6923 <jeyaraman.saravanan23@gmail.com>
2025-02-26 14:44:43 +0530

    cookie script moved

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import HeroSection from "../modules/HeroSection";
 import { Features } from "../modules/Features";
 import { Header } from "../modules/Header";
@@ -12,36 +11,6 @@ import { FAQ } from "../modules/FAQ";
 import Subscribe from "../modules/Subscribe";
 import ContactForm from "../modules/Contact";
 import AboutSection from "../modules/About";
-
-const CookiebotIntegration = () => {
-  useEffect(() => {
-    const existingScript = document.getElementById("Cookiebot");
-    if (!existingScript) {
-      const script = document.createElement("script");
-      script.id = "Cookiebot";
-      script.src = "https://consent.cookiebot.com/uc.js";
-      script.dataset.cbid = "d2d578f4-5959-4eff-bae6-adef05409421";
-      script.dataset.blockingmode = "auto"; // Changed from manual to auto for better placement
-      script.type = "text/javascript";
-      script.async = true;
-      document.body.appendChild(script);
-    }
-
-    const existingDeclaration = document.getElementById("CookieDeclaration");
-    if (!existingDeclaration) {
-      const cookieDeclaration = document.createElement("script");
-      cookieDeclaration.id = "CookieDeclaration";
-      cookieDeclaration.src =
-        "https://consent.cookiebot.com/d2d578f4-5959-4eff-bae6-adef05409421/cd.js";
-      cookieDeclaration.type = "text/javascript";
-      cookieDeclaration.async = true;
-      document.body.appendChild(cookieDeclaration);
-    }
-  }, []);
-
-  return null;
-};
-
 export const Home = () => {
   return (
     <>
@@ -57,8 +26,6 @@ export const Home = () => {
       <AboutSection />
       <ContactForm />
       <Subscribe />
-      <div id="cookie-declaration"></div>
-      <CookiebotIntegration />
       <Footer />
     </>
   );
